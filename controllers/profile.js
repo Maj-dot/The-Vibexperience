accde8991680552938a2d3c5d043fc132f2ff78a
BHerbert <madammaj418@gmail.com>
2024-08-27 02:51:08 -0400

    Update Profile functionality

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const ensureAuthenticated = require('../middleware/ensureLoggedIn');
+const { validationResult } = require('express-validator');
 const User = require('../models/user'); 
 
 router.get('/view', ensureAuthenticated, (req, res) => {
